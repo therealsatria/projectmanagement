89d2a3b1405fae68b9a6240181ff184d11b36f9f
satria n <therealsatria@gmail.com>
2025-02-11 15:44:19 +0700

    update

---
+++ ProjectManagementApi/Repositories/INotebookRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Models;
+
+namespace Repositories
+{
+    public interface INotebookRepository
+    {
+        Task<List<Notebook>> GetAllNotebooksAsync();
+        Task<Notebook> GetNotebookByIdAsync(Guid notebookId);
+        Task AddNotebookAsync(Notebook notebook);
+        Task UpdateNotebookAsync(Notebook notebook);
+        Task DeleteNotebookAsync(Guid notebookId);
+    }
+}
---
+++ ProjectManagementApi/Repositories/NotebookRepository.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Data;
+
+namespace Repositories
+{
+    public class NotebookRepository : INotebookRepository
+    {
+        private readonly AppDbContext _context;
+
+        public NotebookRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Notebook>> GetAllNotebooksAsync()
+        {
+            return await _context.Notebooks.ToListAsync();
+        }
+
+        public async Task<Notebook> GetNotebookByIdAsync(Guid notebookId)
+        {
+            return await _context.Notebooks.FindAsync(notebookId);
+        }
+
+        public async Task AddNotebookAsync(Notebook notebook)
+        {
+            await _context.Notebooks.AddAsync(notebook);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateNotebookAsync(Notebook notebook)
+        {
+            _context.Notebooks.Update(notebook);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteNotebookAsync(Guid notebookId)
+        {
+            var notebook = await _context.Notebooks.FindAsync(notebookId);
+            if (notebook != null)
+            {
+                _context.Notebooks.Remove(notebook);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
